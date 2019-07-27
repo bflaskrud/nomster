1732009f801e181c2547f7d4b0416a89191a97e9
Ben Flaskrud <bflaskrud2@yahoo.com>
2019-07-27 05:24:22 +0000

    caption and photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_24_232130) do
+ActiveRecord::Schema.define(version: 2019_07_27_025437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,11 @@ ActiveRecord::Schema.define(version: 2019_07_24_232130) do
   end
 
   create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
   create_table "places", force: :cascade do |t|
